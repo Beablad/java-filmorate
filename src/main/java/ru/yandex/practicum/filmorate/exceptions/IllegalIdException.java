22d01f34d796829aaa37441072634935b1cc6fe3
Beablad <shu.vivi@yandex.ru>
2022-07-17 14:13:51 +0300

    feat: добавлен ErrorHandler для отлова ошибок

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IllegalIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IllegalIdException.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus (value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class IllegalIdException extends IllegalArgumentException {
 
 }
