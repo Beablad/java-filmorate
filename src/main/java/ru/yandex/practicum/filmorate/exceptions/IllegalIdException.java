2463f3675adb434e67ad59eb2a6a01bd681a2264
Viktor <shu.vivi@yandex.ru>
2022-07-17 23:56:28 +0300

    fix: заменеа аннотация @WebMvcTest на @SpringBootTest @AutoConfigureMockMvc

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/IllegalIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IllegalIdException.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus (value = HttpStatus.NOT_FOUND)
 public class IllegalIdException extends IllegalArgumentException {
 
 }
